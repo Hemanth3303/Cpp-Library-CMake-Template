c3eeca54352edbd277d6c61abfdcccb01dc9516a
Hemanth3303 <109304485+Hemanth3303@users.noreply.github.com>
2026-04-06 14:15:41 +0530

    fixes+updated base that uses new platform config

--- MyLibrary/src/Library.cpp
+++ MyLibrary/src/Library.cpp
@@ -3,11 +3,11 @@
 void Logger::LogMessage(const std::string& message) {
 	std::cout << message << "\n";
 
-#if defined(LIB_DEBUG_BUILD)
+#if defined(MY_LIBRARY_DEBUG_BUILD)
 	std::cout << "Debug Build Library\n";
-#elif defined(LIB_PROFILE_BUILD)
+#elif defined(MY_LIBRARY_PROFILE_BUILD)
 	std::cout << "Profile Build Library\n";
-#elif defined(LIB_RELEASE_BUILD)
+#elif defined(MY_LIBRARY_RELEASE_BUILD)
 	std::cout << "Release Build Library\n";
 #else
 
