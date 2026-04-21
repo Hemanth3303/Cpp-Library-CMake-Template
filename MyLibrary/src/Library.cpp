536dd623d863980fba17cb32fb2ec3f7c25c7f21
Hemanth3303 <109304485+Hemanth3303@users.noreply.github.com>
2026-04-21 12:37:16 +0530

    updated to use new platform and arch defines

--- MyLibrary/src/Library.cpp
+++ MyLibrary/src/Library.cpp
@@ -4,12 +4,46 @@ void Logger::LogMessage(const std::string& message) {
 	std::cout << message << "\n";
 
 #if defined(MY_LIBRARY_DEBUG_BUILD)
-	std::cout << "Debug Build Library\n";
+	std::cout << "MY_LIBRARY: Debug Build\n";
 #elif defined(MY_LIBRARY_PROFILE_BUILD)
-	std::cout << "Profile Build Library\n";
+	std::cout << "MY_LIBRARY: Profile Build\n";
 #elif defined(MY_LIBRARY_RELEASE_BUILD)
-	std::cout << "Release Build Library\n";
+	std::cout << "MY_LIBRARY: Release Build\n";
 #else
-
+	std::cout << "MY_LIBRARY: Unknown Build\n";
 #endif
+
+	std::cout << "PLATFORM: ";
+#if defined(MY_LIBRARY_PLATFORM_WINDOWS)
+	std::cout << "Windows";
+#elif defined(MY_LIBRARY_PLATFORM_LINUX)
+	std::cout << "Linux";
+#elif defined(MY_LIBRARY_PLATFORM_DARWIN)
+	std::cout << "Darwin";
+#elif defined(MY_LIBRARY_PLATFORM_UNKNOWN)
+	std::cout << "Unknown";
+#else
+	std::cout << "Unknown";
+#endif
+	std::cout << "\n";
+
+	std::cout << "ARCHITECTURE: ";
+#if defined(MY_LIBRARY_ARCHITECTURE_X86_64)
+	std::cout << "x86-64";
+#elif defined(MY_LIBRARY_ARCHITECTURE_X86_32)
+	std::cout << "x86-32";
+#elif defined(MY_LIBRARY_ARCHITECTURE_AARCH64)
+	std::cout << "aarch64";
+#elif defined(MY_LIBRARY_ARCHITECTURE_AARCH32)
+	std::cout << "aarch32";
+#elif defined(MY_LIBRARY_ARCHITECTURE_RISCV64)
+	std::cout << "riscv64";
+#elif defined(MY_LIBRARY_ARCHITECTURE_RISCV32)
+	std::cout << "riscv32";
+#elif defined(MY_LIBRARY_ARCHITECTURE_UNKNOWN)
+	std::cout << "unknown";
+#else
+	std::cout << "unknown";
+#endif
+	std::cout << "\n------------------------------\n";
 }
--- SandboxApp/src/SandboxApp.cpp
+++ SandboxApp/src/SandboxApp.cpp
@@ -1,17 +1,52 @@
 #include "Library.hpp"
 
 int main(int argc, char** argv) {
-	Logger::LogMessage("Hello, World");
+	Logger::LogMessage("Hello, World\n------------------------------");
 
 #if defined(SANDBOX_APP_DEBUG_BUILD)
-	std::cout << "Debug Build\n";
+	std::cout << "SANDBOX_APP: Debug Build\n";
 #elif defined(SANDBOX_APP_PROFILE_BUILD)
-	std::cout << "Profile Build\n";
+	std::cout << "SANDBOX_APP: Profile Build\n";
 #elif defined(SANDBOX_APP_RELEASE_BUILD)
-	std::cout << "Release Build\n";
+	std::cout << "SANDBOX_APP: Release Build\n";
 #else
-
+	std::cout << "SANDBOX_APP: Unknown Build\n";
 #endif
+
+	std::cout << "PLATFORM: ";
+#if defined(SANDBOX_APP_PLATFORM_WINDOWS)
+	std::cout << "Windows";
+#elif defined(SANDBOX_APP_PLATFORM_LINUX)
+	std::cout << "Linux";
+#elif defined(SANDBOX_APP_PLATFORM_DARWIN)
+	std::cout << "Darwin";
+#elif defined(SANDBOX_APP_PLATFORM_UNKNOWN)
+	std::cout << "Unknown";
+#else
+	std::cout << "Unknown";
+#endif
+	std::cout << "\n";
+
+	std::cout << "ARCHITECTURE: ";
+#if defined(SANDBOX_APP_ARCHITECTURE_X86_64)
+	std::cout << "x86-64";
+#elif defined(SANDBOX_APP_ARCHITECTURE_X86_32)
+	std::cout << "x86-32";
+#elif defined(SANDBOX_APP_ARCHITECTURE_AARCH64)
+	std::cout << "aarch64";
+#elif defined(SANDBOX_APP_ARCHITECTURE_AARCH32)
+	std::cout << "aarch32";
+#elif defined(SANDBOX_APP_ARCHITECTURE_RISCV64)
+	std::cout << "riscv64";
+#elif defined(SANDBOX_APP_ARCHITECTURE_RISCV32)
+	std::cout << "riscv32";
+#elif defined(SANDBOX_APP_ARCHITECTURE_UNKNOWN)
+	std::cout << "unknown";
+#else
+	std::cout << "unknown";
+#endif
+	std::cout << "\n------------------------------\n";
+	std::cout << "Press Enter to exit...";
 
 	std::cin.get();
 	return 0;
